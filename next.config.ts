2b80e417dad5f8aa9a391dfc99141c17af127f36
PiaoAPCL <48676085+sparkpzch@users.noreply.github.com>
2025-11-23 02:39:19 +0700

    Update github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // If your repository name is not your-username.github.io, uncomment and set basePath
+  // basePath: process.env.NODE_ENV === 'production' ? '/repository-name' : '',
+  // trailingSlash: true,
 };
 
 export default nextConfig;
